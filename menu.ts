f31c3823654ae49add8f9067b5b9c75e1d6aea69
paula-alcantara <paullety.m@gmail.com>
2026-02-04 17:43:03 -0300

    :sparkles: Continuação da Classe Conta

--- menu.ts
+++ menu.ts
@@ -1,81 +1,101 @@
 'use stric'
 
-import leia from 'readline-sync';
 import { Colors } from './src/util/Colors';
 import { Conta } from './src/model/Conta';
+import { Input } from './src/util/Input';
 
-let option: number;
+let operacao: number;
 
+export function main() {
 
-do {
   // Instanciar Objetos da Classe Conta
-
   const c1 = new Conta(1, 1234, 'Paula', 1, 100000.00);
 
   c1.visualizar();
+
+  while(true) {
+
+    console.log(Colors.fg.blue, '\n***************************************');
+    console.log('                                       ');
+    console.log(Colors.fg.bluestrong, '               DIGIBANK                ', Colors.reset);
+    console.log(Colors.fg.blue, '                                       ');
+    console.log('***************************************');
+    console.log('                                       ');
+    console.log('  1 - Criar Conta                      ');
+    console.log('  2 - Listar todas as Contas           ');
+    console.log('  3 - Buscar Conta por Número          ');
+    console.log('  4 - Atualizar Dados da Conta         ');
+    console.log('  5 - Apagar Conta                     ');
+    console.log('  6 - Sacar                            ');
+    console.log('  7 - Depositar                        ');
+    console.log('  8 - Transferir valores entre Contas  ');
+    console.log('  9 - Buscar Conta por Titular         ');
+    console.log('  0 - Finalizar                        ');
+    console.log('                                       ');
+    console.log('***************************************');
+    console.log('                                       ', Colors.reset);
   
-  console.log(Colors.fg.blue, '\n***************************************');
-  console.log('                                       ');
-  console.log(Colors.fg.bluestrong, '               DIGIBANK                ', Colors.reset);
-  console.log(Colors.fg.blue, '                                       ');
-  console.log('***************************************');
-  console.log('                                       ');
-  console.log('  1 - Criar Conta                      ');
-  console.log('  2 - Listar todas as Contas           ');
-  console.log('  3 - Buscar Conta por Número          ');
-  console.log('  4 - Atualizar Dados da Conta         ');
-  console.log('  5 - Apagar Conta                     ');
-  console.log('  6 - Sacar                            ');
-  console.log('  7 - Depositar                        ');
-  console.log('  8 - Transferir valores entre Contas  ');
-  console.log('  9 - Buscar Conta por Titular         ');
-  console.log('  0 - Finalizar                        ');
-  console.log('                                       ');
-  console.log('***************************************');
-  console.log('                                       ', Colors.reset);
-
-  option = leia.questionInt('Escolha a opcao desejada: ')
-
-  if(option === 0) {
-    credits();
-    process.exit(0);
+    operacao = Input.questionInt('Escolha a opcao desejada: ')
+  
+    if(operacao === 0) {
+      sobre();
+      process.exit(0);
+    };
+  
+    switch(operacao) {
+      case 1: 
+        console.log(Colors.fg.whitestrong, '\nCriar Conta', Colors.reset);
+        keyPress();
+      break;
+      case 2: 
+        console.log(Colors.fg.whitestrong, '\nContas Cadastradas', Colors.reset);
+        keyPress();
+      break;
+      case 3: 
+        console.log(Colors.fg.whitestrong, '\nBuscar Conta', Colors.reset);
+        keyPress();
+      break;
+      case 4: 
+        console.log(Colors.fg.whitestrong, '\nAtualizar Dados da Conta', Colors.reset);
+        keyPress();
+      break;
+      case 5: 
+        console.log(Colors.fg.whitestrong, '\nApagar Conta', Colors.reset);
+        keyPress();
+      break;
+      case 6: 
+        console.log(Colors.fg.whitestrong, '\nSacar', Colors.reset);
+        keyPress();
+      break;
+      case 7: 
+        console.log(Colors.fg.whitestrong, '\nDepositar', Colors.reset);
+        keyPress();
+      break;
+      case 8: 
+        console.log(Colors.fg.whitestrong, '\nFazer uma Transferência', Colors.reset);
+        keyPress();
+      break;
+      case 9: 
+        console.log(Colors.fg.whitestrong, '\nBuscar Conta por Titular', Colors.reset);
+        keyPress();
+      break;
+      default:
+        console.log(Colors.fg.red, '\nOperação Inválida!', Colors.reset);
+        keyPress();
+    };
   };
-
-  switch(option) {
-    case 1: 
-      console.log(Colors.fg.whitestrong, 'Criar Conta', Colors.reset);
-    break;
-    case 2: 
-      console.log(Colors.fg.whitestrong, 'Contas Cadastradas', Colors.reset);
-    break;
-    case 3: 
-      console.log(Colors.fg.whitestrong, 'Buscar Conta', Colors.reset);
-    break;
-    case 4: 
-      console.log(Colors.fg.whitestrong, 'Atualizar Dados da Conta', Colors.reset);
-    break;
-    case 5: 
-      console.log(Colors.fg.whitestrong, 'Apagar Conta', Colors.reset);
-    break;
-    case 6: 
-      console.log(Colors.fg.whitestrong, 'Sacar', Colors.reset);
-    break;
-    case 7: 
-      console.log(Colors.fg.whitestrong, 'Depositar', Colors.reset);
-    break;
-    case 8: 
-      console.log(Colors.fg.whitestrong, 'Fazer uma Transferência', Colors.reset);
-    break;
-    case 9: 
-      console.log(Colors.fg.whitestrong, 'Buscar Conta por Titular', Colors.reset);
-    break;
-    default:
-      console.log('Operação Inválida!');
-  }
+  
   
 
-} while(true);
+};
 
-function credits(): void{
+function sobre(): void{
   console.log(Colors.fg.bluestrong, '\nDesenvolvido por Paula Alcantara - alcantara_paula@outlook.com\nGitHub: github.com/alcantarapaula\nLinkedIn: linkedin.com/in/alcantarapaula', Colors.reset)
 };
+
+function keyPress(): void{
+  console.log(Colors.reset, '\nPressione enter para continuar:');
+  Input.prompt();
+}
+
+main();
